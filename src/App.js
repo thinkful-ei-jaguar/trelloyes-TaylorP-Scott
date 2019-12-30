500a60176cf554371a413a18b9e212f9fb3d9f82
Taylor Piccarreto <tpic12@gmail.com>
2019-12-30 16:57:15 -0500

    css styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import List from './List';
-import Card from './Card'
+import './App.css';
 
 function App(props) {
+  //console.log(props);
   return (
     <main className="App">
         <header className="App-header">
           <h1>Trelloyes!</h1>
         </header>
       <div className="App-list">
-      {console.log(props.store.lists[0].id)}
+      {/* {console.log(props.store.lists[0].id)} */}
         {props.store.lists.map(item => (
-            <List key={[item].id} header={item.header} cards={item.cardIds} allCards={props.store.allCards} />
+          // console.log(item),
+            <List key={item.id} header={item.header} cards={item.cardIds} allCards={props.store.allCards} />
         ))}
       </div>
     </main>
